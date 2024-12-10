2dafd87e171b53b3a9dc60074d7dd26b4cb66ffb
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-10 15:56:01 +0530

    PrintNumbers

---
+++ PrintNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class PrintNumbers
+    {
+        //static void Main(string[] args)
+        //{
+        //    int n;
+        //    Console.WriteLine("Enter n value");
+        //    n = int.Parse(Console.ReadLine());
+        //    for (int i = 0; i <= n; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //    Console.ReadLine();
+
+        //}
+    }
+}
