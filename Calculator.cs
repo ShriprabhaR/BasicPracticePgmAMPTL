8c58ff86c6e8481f5a95de42bc75f3f7ebd3b592
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-10 15:31:59 +0530

    Calculater

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class Calculator
+    {
+        static void Main(String[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("enter two numbers");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("sum of two numbers is: " + (a + b));
+            Console.WriteLine("subtraction of two numbers is: " + (a - b));
+            Console.WriteLine("multiplication of two numbers is: " + (a * b));
+            Console.WriteLine("division of two numbers is: " + (a / b));
+            Console.WriteLine("modulus of two numbers is: " + (a % b));
+        }
+    }
+}
