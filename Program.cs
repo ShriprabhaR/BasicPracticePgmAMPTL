f64b165d88beb22d3e6386e624f4e6bdf279050f
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-10 15:49:12 +0530

    Program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter number");
+        a = int.Parse(Console.ReadLine());
+        if (a< 0)
+        {
+            Console.WriteLine("Number is negative");
+        }
+        else if (a >= 0) 
+        {
+            Console.WriteLine("Number is negative");
+        }
+
+Console.ReadLine();
+        }
+    }
+}
