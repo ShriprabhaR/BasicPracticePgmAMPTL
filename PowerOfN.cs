47587ddd07e7848f7916aebcc0028f2689fb98e6
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-10 17:29:14 +0530

    PowerOfN

---
+++ PowerOfN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class PowerOfN
+    {
+        static void Main(string[] args)
+        {
+            int n, p;
+            Console.WriteLine("enter 2 numbers");
+            n = int.Parse(Console.ReadLine());
+            p = int.Parse(Console.ReadLine());
+
+            int prod = 1;
+            for(int i = 0; i < p; i++) 
+            {
+                prod = prod * n;
+            }
+            Console.WriteLine(prod);
+
+        }
+    }
+}
