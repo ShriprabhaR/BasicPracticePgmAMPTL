cf87a68c6e7a20f497355c7c3038fff172411ae2
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-10 15:54:00 +0530

    ReverseNumber

---
+++ ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class ReverseNumber
+    {
+        //static void Main(String[] args)
+        //{
+        //    int n;
+        //    Console.WriteLine("Enter a number to reverse");
+        //    n = int.Parse(Console.ReadLine());
+        //    int rev = 0;
+        //    while(n>0)
+        //    {
+        //        int d = n % 10;
+        //        rev = rev * 10 + d;
+        //        n = n / 10;
+        //    }
+        //    Console.WriteLine("Reversed number: " + rev);
+        //    Console.ReadLine();
+        //}
+    }
+}
