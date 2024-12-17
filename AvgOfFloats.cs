e46a4c24e53152d01235a6b6bee9fa8387b1d600
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-17 17:20:19 +0530

    Average of Float Array

---
+++ AvgOfFloats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class AvgOfFloats
+    {
+        public static double GetAvg(double[] arr)
+        {
+            double sum = 0, avg = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                sum = sum +arr[i];
+
+            }
+            avg = sum / arr.Length;
+            return avg;
+            
+
+        }
+    }
+}
