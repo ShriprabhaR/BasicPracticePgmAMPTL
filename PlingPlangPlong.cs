bfb0a53f46258c2813d358cbd5aff9f0d5d54bc6
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-17 16:37:48 +0530

    it checks for factors of given number

---
+++ PlingPlangPlong.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class PlingPlangPlong
+    {
+        public static void PrintString(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0 && n % 7 == 0)
+            {
+                Console.WriteLine("Pling Plang Plong ");
+            }
+            else if (n % 3 == 0 && n % 5 == 0)
+            {
+                Console.WriteLine("Pling Plang");
+            }
+            else if (n % 3 == 0 && n % 7 == 0)
+            {
+                Console.WriteLine("Pling Plong");
+            }
+            else if (n % 5 == 0 && n % 7 == 0)
+            {
+                Console.WriteLine("Plang Plong");
+            }
+            else if (n % 3 == 0)
+            {
+                Console.WriteLine("Pling");
+            }
+            else if (n % 5 == 0)
+            {
+                Console.WriteLine("Plang");
+            }
+            else
+            {
+                Console.WriteLine("Plong");
+            }
+
+        }
+    }
+}
