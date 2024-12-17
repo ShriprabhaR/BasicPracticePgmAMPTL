36e6e31e3505a58548c0373efb8a55c2b6a3d3e1
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-17 17:24:05 +0530

    Prime Number among integers in array

---
+++ PrimeNumberInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class PrimeNumberInArray
+    {
+        public static int GetPrimeNum(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 2;
+                if (arr[i] %2 == 0)
+                {
+                    count++;
+                }
+                if (count == 2)
+                {
+                    return arr[i];
+                }
+            }
+            return -1;
+            
+        }
+    }
+}
