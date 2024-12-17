83219f2009f7fb158da554b3743647a42d218dc0
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-17 16:41:17 +0530

    it checks for factors of given number

---
+++ CheckPresence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class CheckPresence
+    {
+        public static void CheckIfPresent(String str)
+        {
+             char[] ch = str.ToCharArray();
+            int a = 0, e = 0, p = 0;
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] == 'a')
+                {
+                    a++;
+                }
+                else if (ch[i] == 'e')
+                {
+                    e++;
+                }
+                else if (ch[i] == 'p')
+                {
+                    p++;
+                }
+            }
+
+            if(a!=0 && e!=0 && p != 0)
+            {
+                Console.WriteLine("All present");
+            }
+            else if(a !=0 || e!=0 || p != 0)
+            {
+                Console.WriteLine("One or more present");
+            }
+            else
+            {
+                Console.WriteLine("None Present");
+            }
+
+            
+        }
+    }
+}
