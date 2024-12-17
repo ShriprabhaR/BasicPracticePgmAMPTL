1dac7596f69eb0c513376fc301545a02d86c3808
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-17 16:38:49 +0530

    Without using loop

---
+++ Print1To100.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPracticePgmAMPTL
+{
+    internal class Print1To100
+    {
+        int n = 1;
+        public static void Print(int n)
+        {
+            if (n <= 100)
+            {
+                Console.Write("{0} ", n++);
+                Print(n);
+            }
+        }
+
+    }
+}
